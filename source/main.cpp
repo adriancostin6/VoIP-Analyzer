da5e55040f5d32ecd69b1313573780a4a2e0e2ae
Adrian <adriancostin6@gmail.com>
2020-06-28 04:43:49 +0300

    Change getPorts to get_ports to avoid compile errors

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include <string>
 
 #include "capture.h"
+#include "rtp/decode.h"
 //#include "rtp_decoder/decode.h"
 
 //#include "sip.h"
@@ -27,9 +28,9 @@ int main()
     // config.set_filter("udp[1] & 1 != 1 && udp[3] & 1 != 1 && udp[8] & 0x80 == 0x80 && length < 250");
     config.set_immediate_mode(true);
 
-    PacketCrafter p_craft;
-    p_craft.craft_sip_packet("input", true);
-    p_craft.send_packets();
+//    PacketCrafter p_craft;
+//    p_craft.craft_sip_packet("input", true);
+//    p_craft.send_packets();
 
     try{
 //        std::ifstream ifs("input");
@@ -54,26 +55,25 @@ int main()
 //
 //        capture_all.join();
 //
-//        // parse the pcap file and extract the sip data
-//        Capture capture_sip(Capture::IS_SIP,"sip");
-//        config.set_filter("port 5060");
-//        Tins::FileSniffer fsniffer_sip("../temp/all.pcap",config);
-//        capture_sip.run_file_sniffer(fsniffer_sip);
-//        //write each packet to output directory
-//        std::string op_name = "../outputs/packet_";
-//        capture_sip.print(op_name);
-//
-//        //parse pcap file and extract the rtp data
-//        Capture capture_rtp(Capture::IS_RTP,"rtp");
-//        config.set_filter("udp[1] & 1 != 1 && udp[3] & 1 != 1 && udp[8] & 0x80 == 0x80 && length < 250");
-//        Tins::FileSniffer fsniffer_rtp("../temp/all.pcap",config);
-//        capture_rtp.run_file_sniffer(fsniffer_rtp);
-//
-//
-//
-//        //get ports and ips needed for RTP decoding 
-//        auto ports_and_ips = capture_rtp.getPorts();
-//        std::string server_ip = "192.168.1.8";
+        // parse the pcap file and extract the sip data
+        Capture capture_sip(Capture::IS_SIP,"sip");
+        config.set_filter("port 5060");
+        Tins::FileSniffer fsniffer_sip("../temp/all.pcap",config);
+        capture_sip.run_file_sniffer(fsniffer_sip);
+        //write each packet to output directory
+        std::string op_name = "../outputs/packet_";
+        capture_sip.print(op_name);
+
+        //parse pcap file and extract the rtp data
+        Capture capture_rtp(Capture::IS_RTP,"rtp");
+        config.set_filter("udp[1] & 1 != 1 && udp[3] & 1 != 1 && udp[8] & 0x80 == 0x80 && length < 250");
+        Tins::FileSniffer fsniffer_rtp("../temp/all.pcap",config);
+        capture_rtp.run_file_sniffer(fsniffer_rtp);
+        
+
+        //get ports and ips needed for RTP decoding 
+        auto ports_and_ips = capture_rtp.get_ports();
+        std::string server_ip = "192.168.1.8";
 //
 //        //keep this for later logging 
 //        //for(auto& key_pair : ports_and_ips)
@@ -85,33 +85,33 @@ int main()
 //        //    std::cout << "Source Port: "<< port_pair.first << " Destination Port: "<< port_pair.second<< "\n";
 //        //}
 //
-//        for(auto it=ports_and_ips.begin(); it!=ports_and_ips.end();)
-//        {
-//            if(it->first.first == server_ip)
-//                ports_and_ips.erase(it++);
-//            else
-//                ++it; 
-//        }
-//
-//        std::cout << "Pairs you were looking for \n";
-//        for(auto& key_pair : ports_and_ips)
-//        {
-//            auto ip_pair = key_pair.first;
-//            auto port_pair = key_pair.second;
-//
-//            std::cout << "Source IP: "<< ip_pair.first << " Destination IP: "<< ip_pair.second<< "\n";
-//            std::cout << "Source Port: "<< port_pair.first << " Destination Port: "<< port_pair.second<< "\n";
-//        }
-//        int i = 1;
-//        for(auto& key_pair : ports_and_ips)
-//        {
-//            std::string out_filename = "Speaker_" + std::to_string(i);
-//            auto ip_pair = key_pair.first;
-//            auto port_pair = key_pair.second;
-//            
-//            decode("../temp/rtp.pcap", out_filename, ip_pair.first, ip_pair.second, port_pair.first, port_pair.second);
-//            i++;
-//        }
+        for(auto it=ports_and_ips.begin(); it!=ports_and_ips.end();)
+        {
+            if(it->first.first == server_ip)
+                ports_and_ips.erase(it++);
+            else
+                ++it; 
+        }
+
+        std::cout << "Pairs you were looking for \n";
+        for(auto& key_pair : ports_and_ips)
+        {
+            auto ip_pair = key_pair.first;
+            auto port_pair = key_pair.second;
+
+            std::cout << "Source IP: "<< ip_pair.first << " Destination IP: "<< ip_pair.second<< "\n";
+            std::cout << "Source Port: "<< port_pair.first << " Destination Port: "<< port_pair.second<< "\n";
+        }
+        int i = 1;
+        for(auto& key_pair : ports_and_ips)
+        {
+            std::string out_filename = "Speaker_" + std::to_string(i);
+            auto ip_pair = key_pair.first;
+            auto port_pair = key_pair.second;
+
+            decode("../temp/rtp.pcap", out_filename, ip_pair.first, ip_pair.second, port_pair.first, port_pair.second);
+            i++;
+        }
     }catch(const std::string& sip_ex){
         std::cout << "Error crafting SIP packet: ";
         std::cout << sip_ex << "\n";
